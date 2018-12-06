f8b57d1a96fa6975957242c0b413a37c6ac05999
Hannah Schinz <kimbumisso@gmail.com>
2018-12-06 19:47:58 +0100

    Initial project setup using a less port for bootstrap v4 with angular-cli

---
+++ src/app/shared/components/navigation/navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nb-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.less']
+})
+export class NavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
