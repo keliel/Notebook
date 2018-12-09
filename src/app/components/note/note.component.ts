4004a632edb28c559487d7ac0f1ef24cf162350f
Hannah Schinz <kimbumisso@gmail.com>
2018-12-09 15:17:28 +0100

    Update existing note

--- src/app/components/note/note.component.ts
+++ src/app/components/note/note.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Note } from 'src/app/shared/models/note';
+import { LocalStorageService } from 'src/app/shared/services/storage.service';
 
 @Component({
   selector: 'nb-note',
@@ -9,8 +10,9 @@ import { Note } from 'src/app/shared/models/note';
 export class NoteComponent {
 
   @Input() note: Note;
-
   editMode = false;
+
+  constructor(private storageService: LocalStorageService) { }
 
   edit(): void {
     this.editMode = true;
@@ -19,5 +21,11 @@ export class NoteComponent {
   onEditClose(noteText: string): void {
     this.editMode = false;
     this.note.text = noteText;
+
+    // Update in local storage
+    let storedNotes = this.storageService.get<Array<Note>>('NOTES');
+    const storedNoteIndex = storedNotes.indexOf(storedNotes.find(x => x.id === this.note.id));
+    storedNotes[storedNoteIndex] = this.note;
+    this.storageService.set<Array<Note>>('NOTES', storedNotes);
   }
 }
