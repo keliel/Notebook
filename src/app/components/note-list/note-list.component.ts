b5670082e9970591caf1c61d137546a26a90d286
Hannah Schinz <kimbumisso@gmail.com>
2018-12-09 17:46:09 +0100

    Sorting for notes list

--- src/app/components/note-list/note-list.component.ts
+++ src/app/components/note-list/note-list.component.ts
@@ -11,12 +11,40 @@ export class NoteListComponent implements OnInit {
 
   @Input() noteList: Array<Note>;
   sortModeValues = SortMode;
-  sortModes : Array<string> = Object.keys(SortMode);
+  sortModes: Array<string> = Object.keys(SortMode);
   currentSortMode = SortMode.Ascending;
 
   constructor() { }
 
   ngOnInit() {
+    this.sortNotesList(this.currentSortMode);
+  }
+
+  onSortChanged(sortMode: SortMode): void {
+    this.currentSortMode = sortMode;
+    this.sortNotesList(this.currentSortMode);
+  }
+
+  private sortNotesList(sortMode: SortMode): void {
+    let sortingFn = (() => {
+      switch (sortMode) {
+        case SortMode.Ascending:
+          return this.sortByAsc;
+        case SortMode.Descending:
+          return this.sortByDesc;
+        default:
+          return this.sortByAsc;
+      }
+    })();
+    this.noteList.sort(sortingFn);
+  }
+
+  private sortByAsc(a: Note, b: Note) {
+    return a.text.localeCompare(b.text);
+  }
+
+  private sortByDesc(a: Note, b: Note) {
+    return b.text.localeCompare(a.text);
   }
 
 }
