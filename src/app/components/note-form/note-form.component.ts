e27748211f80739ed3e56cf5c03d9e3a1fe715ca
Hannah Schinz <kimbumisso@gmail.com>
2018-12-09 12:42:24 +0100

    Differentiate between old and new value

--- src/app/components/note-form/note-form.component.ts
+++ src/app/components/note-form/note-form.component.ts
@@ -1,23 +1,38 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'nb-note-form',
   templateUrl: './note-form.component.html',
   styleUrls: ['./note-form.component.less']
 })
-export class NoteFormComponent {
+export class NoteFormComponent implements OnInit {
 
   @Input() note: string;
   @Input() addMode = false;
   @Output() closeEdit = new EventEmitter<string>();
 
+  model: string;
+
   constructor() { }
 
+  ngOnInit(): void {
+    this.model = this.note;
+  }
+
+  /**
+   * Submit current input, if there is any change.
+   * Definitely emit closeEdit event.
+   */
   submit(): void {
-    if (this.note && this.note.length > 0) {
+    let result = this.note;
+
+    // Check if there is input and if it is different from before
+    if (this.model && this.model.length > 0 && this.model !== this.note) {
       //TODO: Save note, before emitting event to close edit mode.
-      this.closeEdit.emit(this.note);
-      this.note = null;
+      result = this.model;
     }
+
+    this.closeEdit.emit(result);
+    this.model = null;
   }
 }
