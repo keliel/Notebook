4531f46870f6985d6d76cb7af3b3cdfe7a3c6431
Hannah Schinz <kimbumisso@gmail.com>
2018-12-09 14:07:16 +0100

    Add notes to storage and retrieve them

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Note } from './shared/models/note';
+import { LocalStorageService } from './shared/services/storage.service';
+
+const NOTE_STORAGE_KEY = 'NOTES';
 
 @Component({
   selector: 'nb-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   notes: Array<Note> = [];
+
+  constructor(private storageService: LocalStorageService) { }
+
+  ngOnInit(): void {
+    const storedNotes = this.storageService.get<Array<Note>>(NOTE_STORAGE_KEY);
+    this.notes.push(...storedNotes);
+  }
 
   onAdd(newNoteText: string): void {
     this.notes.push({ text: newNoteText });
+    this.storageService.set<Array<Note>>(NOTE_STORAGE_KEY, this.notes);
   }
 }
