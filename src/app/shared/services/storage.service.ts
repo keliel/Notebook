b997a8a85354bd4ac8963ae28b2f7b610cf22da3
Hannah Schinz <kimbumisso@gmail.com>
2018-12-09 13:43:44 +0100

    Storage service content

--- src/app/shared/services/storage.service.ts
+++ src/app/shared/services/storage.service.ts
@@ -3,5 +3,36 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class LocalStorageService {
 
-  constructor() { }
+  private static store: Storage = window.localStorage;
+
+  /**
+   * Stores a value by key in local storage
+   *
+   * @param key Key of the entry
+   * @param value Value of the entry
+   */
+  set<T>(key: string, value: T): T {
+    const serializedData = JSON.stringify(value);
+    LocalStorageService.store.setItem(key, serializedData);
+    return value;
+  }
+
+  /**
+   * Retrieve an entry by key out of local storage
+   *
+   * @param key Key of the entry
+   */
+  get<T = any>(key: string): T {
+    const serializedData = LocalStorageService.store.getItem(key);
+    return JSON.parse(serializedData);
+  }
+
+  /**
+   * Removes the entry identified by the given key from local storage
+   *
+   * @param key Key of the entry
+   */
+  delete(key: string): void {
+    LocalStorageService.store.removeItem(key);
+  }
 }
